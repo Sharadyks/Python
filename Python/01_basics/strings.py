ac09a6d87108228d912a1da007bdad7fd713b8c1
Sharad <ksfbd100@gmail.com>
2025-08-30 22:30:35 +0530

    more studied about strings

---
+++ Python/01_basics/strings.py
@@ -0,0 +1,8 @@
+chai = "Masala Chai"
+print(chai.strip())
+print(chai.replace("Chai","less"))
+
+chai_type = "Lemon"
+quantity = 2
+order = "I ordered {} cups of {} chai"
+print(order.format(quantity,chai_type))
