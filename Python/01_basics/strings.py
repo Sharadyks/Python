e2fac7c9e20f6c059fa5fdbcd0340125ff4f9324
Sharad <ksfbd100@gmail.com>
2025-08-31 12:54:53 +0530

    more functions of strings

--- Python/01_basics/strings.py
+++ Python/01_basics/strings.py
@@ -1,4 +1,5 @@
 chai = "Masala Chai"
+name = "Sharad Sharma"
 print(chai.strip())
 print(chai.replace("Chai","less"))
 
@@ -6,3 +7,12 @@ chai_type = "Lemon"
 quantity = 2
 order = "I ordered {} cups of {} chai"
 print(order.format(quantity,chai_type))
+print(chai.split())
+print(chai.upper())
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+print(name.find("Sha"))
+print(name.index("ra"))
+print(name.startswith("S"))
+print(name.count("a"))
