7911ddd3e290ecde7470fc6b1c03518858fc8a8f
Sharad <ksfbd100@gmail.com>
2025-09-02 14:00:05 +0530

    practice questions on dictionary and set

---
+++ Python/PS Chapter 5/Problem1.py
@@ -0,0 +1,10 @@
+words = {
+    "Good morning" : "Suprabhat",
+    "Mistake" : "Galti",
+    "Decision" : "Nirnay",
+    "Condition" : ["Haalat", "Shart"]
+
+}
+word = input("Enter the word: ")
+print(words[word])
+ 
